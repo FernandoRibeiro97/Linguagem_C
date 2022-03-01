3071de7a0eb4559472a79ba83573a974be3b9456
Fernando Ribeiro <fernandophanton@hotmail.com>
2022-03-01 17:46:08 -0300

    commit exercicios

---
+++ Exercícios/Capítulo 01/ex03/prog0103.cpp
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+main(){
+	printf("Utilizando a quebra de linha ..\n");
+	printf("Hello World !\n");
+}
---
+++ Exercícios/Capítulo 01/ex05/prog0105.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <locale.h> /*inclui a biblioteca locale*/
+
+main(){
+	setlocale(LC_ALL, "portuguese-brazilian"); /*Definindo UTF-8*/
+	
+	printf("Utilizando aspas dentro de uma string\n");
+	printf("O sol está \"LINDO\" hoje!\n");
+	printf("\7");
+}
